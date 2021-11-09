8928b95227a83664514926f52282fb1e53774e01
Ramzi <ramzi@dataddo.com>
2021-11-09 13:48:30 +0100

    Uploaded files

---
+++ pages/About.vue
@@ -0,0 +1,16 @@
+<template>
+<main>
+    <h1 class="font-bold text-xl">This is the about page</h1>
+    <a href='/'>Homepage</a>
+</main>
+</template>
+
+<style scoped>
+main {
+    @apply text-center pt-8;
+}
+a {
+    @apply text-blue-500 underline
+
+}
+</style>
---
+++ pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+<main>
+    <h1 class="font-bold text-xl">This is the home page</h1>
+    <a href='/about'>About</a>
+</main>
+</template>
+
+<style scoped>
+main {
+    @apply text-center pt-8;
+}
+a {
+    @apply text-blue-500 underline
+}
+</style>
+
