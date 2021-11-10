4cc1e4b100e0ec53c89b459edd6c7abf00ed1b98
Ramzi <ramzi@dataddo.com>
2021-11-11 00:28:30 +0100

    New Deploy

---
+++ assets/Contact.14d32684.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.b718330c.js";import{o as n,e as o}from"./vendor.1a998ac1.js";const r={};function t(c,a){return n(),o("h1",null,"Contact Page Under Construction ...")}var f=e(r,[["render",t]]);export{f as default};
