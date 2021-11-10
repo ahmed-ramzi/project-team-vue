f8924dd6e6471869003c1bf2b7f292d0f6106d83
Ramzi <ramzi@dataddo.com>
2021-11-10 22:17:23 +0100

    New Deploy

---
+++ dist/assets/Profolio.15f6f6eb.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./index.d39855f6.js";import{o as e,e as r}from"./vendor.1a998ac1.js";const n={};function c(t,a){return e(),r("h1",null,"Profolio Page Under Construction ...")}var _=o(n,[["render",c]]);export{_ as default};
